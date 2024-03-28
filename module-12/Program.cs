13410319e8daf6d059e9c7d7b1bfffce25c14e62
youknovv <99376197+youknovv@users.noreply.github.com>
2024-03-29 03:12:29 +0700

    addd hw

---
+++ module-12/Program.cs
@@ -0,0 +1,35 @@
+﻿class User
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public bool IsPremium { get; set; }
+}
+
+class Program
+{
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        Thread.Sleep(3000);
+    }
+
+    static void Greetings(List<User> users)
+    {
+        foreach (User user in users)
+        {
+            Console.WriteLine($"Здравствуйте, {user.Name}!");
+
+            if (!user.IsPremium)
+            {
+                ShowAds();
+            }
+        }
+    }
+}
+
